0ccce8138ee6eabc63087b3a1584df64024391aa
hamed-deriv <57184669+hamed-deriv@users.noreply.github.com>
2022-04-15 09:24:20 +0800

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_audio_player/home_page.dart';
+
+void main() => runApp(const App());
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Audio Player',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(title: 'Flutter Audio Player'),
+    );
+  }
+}
